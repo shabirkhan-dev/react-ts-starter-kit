fde1223e1831daaafede2614fc4161642e8d5ff6
shabirkhan-dev <135199362+shabirkhan10@users.noreply.github.com>
2024-11-23 02:58:22 +0500

    feat: add a example navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,35 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { routeTree } from "./routeTree.gen";
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // Create a new router instance
-const router = createRouter({ routeTree, context: { queryClient } })
+const router = createRouter({ routeTree, context: { queryClient } });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   namespace NodeModule {
     interface Modulo {
-      router: typeof router
+      router: typeof router;
     }
   }
 }
 
-
 // Render the app
-const rootElement = document.getElementById('root')!
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement)
+  const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-			</QueryClientProvider>
-    </StrictMode>,
-  )
+      </QueryClientProvider>
+    </StrictMode>
+  );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,42 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
+const Nav = () => (
+  <div className="mx-12 px-4 py-2 my-4 flex justify-between rounded-full dark:bg-zinc-800 bg-zinc-100">
+    <h1 className="text-lg font-semibold">
+      <Link to="/" className="[&.active]:font-bold [&.active]:text-avocado-500">
+        Avocado
+      </Link>
+    </h1>
+    <div className="flex gap-2">
+      <Link to="/" className="[&.active]:font-bold">
+        Home
+      </Link>
+      <Link to="/about" className="[&.active]:font-bold">
+        About
+      </Link>
+    </div>
+    <div className="flex gap-2">
+      <button
+        type="button"
+        className="bg-avocado-500 text-white px-3 cursor-pointer py-1 rounded-full"
+      >
+        Login
+      </button>
+    </div>
+  </div>
+);
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
+      <Nav />
+      <div className="mx-12">
         <Outlet />
+      </div>
       <ReactQueryDevtools initialIsOpen={false} />
-
       <TanStackRouterDevtools />
     </>
   ),
-})
+});
