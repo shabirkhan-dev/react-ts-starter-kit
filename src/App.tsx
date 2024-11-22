17153c6ed71934fc8502ec471a5f0748c29f1130
shabirkhan-dev <135199362+shabirkhan10@users.noreply.github.com>
2024-11-23 01:18:18 +0500

    feat: add tailwindcss v4 and biome js for linting as well lefthook as precommit hoook

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return <div className="text-3xl font-bold underline">
+    <div className="perspective-distant">
+        <article className="... transform-3d rotate-x-51 rotate-z-43 shadow-xl transition-all duration-500 hover:-translate-y-4 hover:rotate-x-49 hover:rotate-z-38 hover:shadow-2xl">
+          Hello World
+        </article>
+      </div>
+  </div>;
+};
+
+export default App;
